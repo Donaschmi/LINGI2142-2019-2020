855fced0355c18d85d6d24d80243a075cf31034c
Vagrant Default User <vagrant@group8>
2019-11-10 13:55:46 +0000

    Finish pinging test

--- tests/tests.py
+++ tests/tests.py
@@ -10,6 +10,8 @@ def main(network):
 
 
 def test_ping_google(network):
+    failed = 0
+    succeed = 0
     for r in network.routers:
         router = network.routers[r]
         r_name = router.name
@@ -17,12 +19,19 @@ def test_ping_google(network):
         child = pexpect.spawn('sudo ../connect_to.sh ' + network.topo + ' ' + r_name)
 
         child.sendline('ping6 2001:4860:4860::8888 -c 1')
-        idx = child.expect(['0% packet loss', r'\d+% packet loss'])
+        idx = child.expect(['0% packet loss', r'\d+% packet loss', 'connect: Network is unreachable'])
         if idx == 0:
             print(r_name + " - Ping of Google successfull")
+            succeed += 1
         else:
             print(r_name + " - Ping of Google failed")
+            failed += 1
         child.sendline('exit')
+    ratio = (succeed / (succeed + failed)) * 100
+    print("Ran ping test for", str(len(network.routers)), "routers :")
+    print("Success : " , succeed)
+    print("Fail : " , failed)
+    print("Ratio : " , ratio , "%")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
